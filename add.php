f8c07521306805b593c75a6f691303622303333a
Netta <c1nine00@students.oamk.fi>
2022-11-08 14:28:37 +0200

    New try

--- add.php
+++ add.php
@@ -1,21 +1,20 @@
-<?php
-header('Access-Control-Allow-Origin: *');
-header('Content-Type: application/json');
-
-$input = json_decode(file_get_contents('php://input'));
-$amount = filter_var($input->amount,FILTER_SANITIZE_STRING);
-$description = filter_var($input->description,FILTER_UNSAFE_RAW);
-
-try {
-  $db=openDb();
-  $query = $db->prepare('insert into item(description,amount) values (:description,:amount)');
-  $query->bindValue(':description', $description,PDO::PARAM_STR);
-  $query->bindValue(':amount', $amount,PDO::PARAM_INT);
-  $query->execute();
-  header('HTTP/1.1 200 OK');
-  $data = array('id' => $db->lastInsertId(),'description' => $description,'amount' => $amount);
-  print json_encode($data);
-
-} catch (PDOException $pdoex) {
-  returnError($pdoex);
+<?php 
+require_once 'inc/functions.php'; 
+require_once 'inc/headers.php'; 
+ 
+$input = json_decode(file_get_contents('php://input')); 
+$description = filter_var($input->description,FILTER_UNSAFE_RAW); 
+$amount = filter_var($input->amount,FILTER_SANITIZE_SPECIAL_CHARS); 
+ 
+try { 
+  $db=openDb(); 
+  $query = $db->prepare('insert into item(description,amount) values (:description,:amount)'); 
+  $query->bindValue(':description', $description,PDO::PARAM_STR); 
+  $query->bindValue(':amount', $amount,PDO::PARAM_INT); 
+  $query->execute(); 
+  header('HTTP/1.1 200 OK'); 
+  $data = array('id' => $db->lastInsertId(),'description' => $description,'amount' => $amount); 
+  print json_encode($data); 
+} catch (PDOException $pdoex) { 
+  returnError($pdoex); 
   }
--- index.php
+++ index.php
@@ -1,15 +1,15 @@
-<?php
-header('Acess-Control-Allow-Origin:*');
-header('Content-Type: application/json');
-
-try{
-    $db=openDb();                        
-    $sql="select * from item";
-    $query = $db->query($sql);                          
-    $results = $query->fetchAll(PDO::FETCH_ASSOC);         
-    header('HTTP/1.1 200 OK');                         
-    print json_encode($results);  
-
-    } catch (PDOException $pdoex) {
-    returnError($pdoex);
+<?php 
+require_once 'inc/functions.php'; 
+require_once 'inc/headers.php'; 
+ 
+try{ 
+$db=openDb();                         
+$sql="select * from item"; 
+$query = $db->query($sql);                           
+$results = $query->fetchAll(PDO::FETCH_ASSOC);       
+$json = json_encode($results,JSON_PRETTY_PRINT);     
+header('HTTP/1.1 200 OK');                          
+print json_encode($results);                                        
+} catch (PDOException $pdoex) { 
+    returnError($pdoex); 
     }
